08a5f629e881db2c9f2d123336fb0904eb8a5433
Diego Hernandez <diegofootball19@gmail.com>
2023-01-03 20:42:58 -0600

    added new stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./pages/*.html"],
   theme: {
     extend: {
       fontFamily: {
-        'body': ['"Porsche Next"'],
-      }
+        body: ['"Porsche Next"'],
+      },
+      keyframes: {
+        fade: {
+          "0%": { transform: "scale(0.8)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        wave: "fade 1s ease-in 1",
+      },
     },
+    plugins: [],
   },
-  plugins: [],
-}
+};
